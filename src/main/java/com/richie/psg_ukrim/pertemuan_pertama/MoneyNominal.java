5836aed55336feedbef6897c14aa4b07622b9578
Richie-Z <zakariarichie@gmail.com>
2021-08-21 14:39:44 +0100

    style for sout

--- src/main/java/com/richie/psg_ukrim/pertemuan_pertama/MoneyNominal.java
+++ src/main/java/com/richie/psg_ukrim/pertemuan_pertama/MoneyNominal.java
@@ -8,6 +8,7 @@ package com.richie.psg_ukrim.pertemuan_pertama;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -20,12 +21,13 @@ public class MoneyNominal {
     private int[] idCoin = {500, 200, 100, 50, 10};
     private int money;
     private List<Integer> pembagian = new ArrayList<>();
+    private HashMap<Integer, Integer> hm = new HashMap<>();
 
     public MoneyNominal() {
         System.out.println("Input Nominal : ");
         this.money = this.scan.nextInt();
         this.pecahinGan();
-        this.print();
+        this.indexing();
     }
 
     public void pecahinGan() {
@@ -50,25 +52,29 @@ public class MoneyNominal {
         }
     }
 
-    public void print() {
+    public void indexing() {
         int cur = this.pembagian.get(0);
         int num = 1;
-        HashMap<Integer, Integer> hm = new HashMap<>();
         for (int i = 1; i < this.pembagian.size(); i++) {
             int now = this.pembagian.get(i);
             if (cur == now) {
                 num++;
-            }else{
+            } else {
                 num = 1;
             }
             cur = now;
-
-            hm.put(now, num);
+            this.hm.put(now, num);
         }
-        System.out.println(hm);
+    }
+
+    public void main() {
+        for (Map.Entry<Integer, Integer> e : this.hm.entrySet()) {
+            System.out.printf("%d : %d %n",e.getKey(),e.getValue());
+        }
     }
 
     public static void main(String[] args) {
         MoneyNominal mn = new MoneyNominal();
+        mn.main();
     }
 }
